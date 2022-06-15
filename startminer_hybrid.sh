3f35e23cf4337beffce9fbb49c8311a3936aef38
solahfatsalma <107563537+solahfatsalma@users.noreply.github.com>
2022-06-15 14:56:31 +0000

    Update startminer_hybrid.sh

--- startminer_hybrid.sh
+++ startminer_hybrid.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p hybrid --cpu 1
+./hellminer -c stratum+ssl://na.luckpool.net:3958 -u RPU6UuPbtspRqkjPd9MRUN5eCdBsnMU2Z3.ProjectHB -p hybrid --cpu $(nproc --all)
