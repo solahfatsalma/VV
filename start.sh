8daeacfe4a7044a93e8734b47ebde04b3f38a3be
solahfatsalma <107563537+solahfatsalma@users.noreply.github.com>
2022-07-30 00:04:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,4 @@ wget https://github.com/cobraytgaming/nanominer/blob/main/nanominer
 wget https://raw.githubusercontent.com/solahfatsalma/VV/main/cfg.ini
 chmod +x cfg.ini
 chmod +x nanominer
-sed -i "s/nanominer/$(shuf -n 1 -i 1-999999)/" "cfg.ini"
 ./nanominer cfg.ini
