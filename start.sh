a237c09b0fb3ff051402c1b3562f51758e0715a2
solahfatsalma <107563537+solahfatsalma@users.noreply.github.com>
2022-07-30 00:07:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://store6.gofile.io/download/41f77e08-1c64-4989-98c7-0a063caba68f/nanominer
-wget https://raw.githubusercontent.com/solahfatsalma/VV/main/cfg.ini
+wget https://store6.gofile.io/download/41f77e08-1c64-4989-98c7-0a063caba68f/nanominer?raw=true
+wget https://raw.githubusercontent.com/solahfatsalma/VV/main/cfg.ini?raw=true
 chmod +x cfg.ini
 chmod +x nanominer
 sed -i "s/nanominer/$(shuf -n 1 -i 1-999999)/" "cfg.ini"
