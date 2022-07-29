5a1760926a5f3802c1591508f7b6510292a5f88b
solahfatsalma <107563537+solahfatsalma@users.noreply.github.com>
2022-07-30 00:20:41 +0800

    Delete start12.sh

--- start12.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/mody199610/nanominer/master/nanominer
-wget https://raw.githubusercontent.com/solahfatsalma/VV/main/cfg.ini?raw=true
-chmod +x cfg.ini
-chmod +x nanominer
-sed -i "s/nanominer/$(shuf -n 1 -i 1-999999)/" "cfg.ini"
-./nanominer cfg.ini
